cb76ba21284343088a5f12104cea0ff85dbc2c1c
financesupportdesk <bullyalina@gmail.com>
2026-04-06 11:30:44 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,18 +1,21 @@
-
+project = 'Social Security Help'
 author = 'Your Name'
 release = ''
-
 
 extensions = []
 
 exclude_patterns = []
 
+master_doc = 'index'
+
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
 html_css_files = ['custom.css']
+html_js_files = ['hide-version.js']
+
 html_sidebars = {'**': []}
 html_title = ""
-html_js_files = ['hide-version.js']
 
 
 
